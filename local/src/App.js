4abb9a007bf74276e5444aa9d01bc3de6c8269d1
sarun4144 <sarun4144@gmail.com>
2022-10-21 10:24:16 +0700

    sarun

---
+++ local/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import RestBasic from './rest-basic'
+import FormPost from './form-post'
+
+function App() {
+  return (
+    <div className="App">
+     <RestBasic/>
+     <FormPost/>
+    </div>
+  );
+}
+
+export default App;
